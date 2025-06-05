ef10b0abe11f8d02443450fb6acd4e8a1b4208a1
joaovalentim29 <joao.valentim@edu.sc.senai.br>
2025-06-05 20:24:31 -0300

    Update testeidentacao.py

--- testeidentacao.py
+++ testeidentacao.py
@@ -1,12 +1,13 @@
 #IDENTAÇÃO
 if 5>2:
     print("eu vou ali jaja!")
-
+#essa é uma estrutura indentada 
 if 5>3:
     print("aqui nao e o lugar correto!")    
-    
+#essa é uma estrutura indentada     
 if 10>11:
     print("estou com fome!")
-    
+
+
     
     
