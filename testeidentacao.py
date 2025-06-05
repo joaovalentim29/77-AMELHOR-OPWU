6f9140c81778689739a1c74f4d468a37ceccc9f7
joaovalentim29 <joao.valentim@edu.sc.senai.br>
2025-06-05 20:00:00 -0300

    meuprimeirocommit
    estou versionando

---
+++ testeidentacao.py
@@ -0,0 +1,10 @@
+#IDENTAÇÃO
+if 5>10:
+    print("eu vou ali jaja!")
+
+
+print("aqui nao e o lugar correto!")    
+    
+    
+    
+    
---
+++ umanoite77.py
@@ -0,0 +1,4 @@
+print("LIVRO:")
+print("Uma noite na 77")
+print("DESCRIÇÃO:")
+print("Este livro relata a experiencia de um professor em uma noite com a turma 77!")
