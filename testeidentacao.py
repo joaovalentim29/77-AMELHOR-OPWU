906737864e0669393b508c0d1b8f6b55d8792130
joaovalentim29 <joao.valentim@edu.sc.senai.br>
2025-06-05 20:20:46 -0300

    2 commit
    minha 2 vez verminando

--- testeidentacao.py
+++ testeidentacao.py
@@ -1,10 +1,12 @@
 #IDENTAÇÃO
-if 5>10:
+if 5>2:
     print("eu vou ali jaja!")
 
-
-print("aqui nao e o lugar correto!")    
+if 5>3:
+    print("aqui nao e o lugar correto!")    
     
+if 10>11:
+    print("estou com fome!")
     
     
     
